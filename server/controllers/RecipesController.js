91a8923e038046d860dc33b4325913afcf485754
NS <noyshlomo2004@gmail.com>
2024-09-07 20:39:50 +0300

    added delete and update controllers functions

--- server/controllers/RecipesController.js
+++ server/controllers/RecipesController.js
@@ -24,7 +24,7 @@ const getAllUserRecipes = async (req, res) => {
     }
 
     // Fetch recipes from the database
-    const recipe = await RecipesDB.find({ userId: id })
+    const recipe = await RecipesDB.find({ _id: id })
 
     // If no recipes found, return 404
     if(!recipe){
@@ -35,7 +35,68 @@ const getAllUserRecipes = async (req, res) => {
     res.status(200).json(recipe)
 }
 
+// delete a recipe
+const deleteRecipe = async (req, res) => {
+    const { id } = req.params
+
+    // Validate recipe ID
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'Invalid recipe ID'})
+    }
+
+    try {
+        // Attempt to find and delete the recipe by recipe ID
+        const recipe = await RecipesDB.findOneAndDelete({_id: id})
+
+        // If no recipe is found, return 404
+        if(!recipe){
+            return res.status(404).json({error: 'No such recipe'})
+        }
+
+        // Success: return a confirmation message
+        res.status(200).json({ message: 'Recipe deleted successfully' });
+    }catch(error){
+        // Catch any server errors
+        res.status(500).json({ error: 'Server error' });
+    }
+}
+
+//update a recipe
+const updateRecipe = async (req, res) => {
+    const { id } = req.params
+
+    // Validate recipe ID
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No such recipe'})
+    }
+
+     // Check if request body contains update data
+    if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({ error: 'No update data provided' });
+    }
+    try {
+        // Attempt to find and update the recipe by recipe ID
+        const recipe = await RecipesDB.findOneAndUpdate({ _id: id }, {
+            ...req.body
+        }, { new: true });
+
+        // If no recipe is found, return 404
+        if (!recipe) {
+            return res.status(404).json({ error: 'No such recipe' });
+        }
+
+        // Return the updated recipe
+        res.status(200).json(recipe);
+    } catch (error) {
+        // Catch any server errors
+        res.status(500).json({ error: 'Server error' });
+    }
+}
+
+
 module.exports = {
     getAllRecipes,
-    getAllUserRecipes
+    getAllUserRecipes,
+    deleteRecipe,
+    updateRecipe
 }
