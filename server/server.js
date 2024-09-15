2d8c6e1d0a56965ae22b8a5d56b29c29297768ed
renaswaid <renaswaid1@gmail.com>
2024-09-15 21:24:22 +0300

    changing

--- server/server.js
+++ server/server.js
@@ -2,21 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-<<<<<<< HEAD
-<<<<<<< HEAD
-const usersRoutes = require('./routes/user')
-const recipesRouter = require ('./routes/recipesRouter')
-=======
-const recipeRouter = require ('./routes/recipeRouter')
->>>>>>> e7edea3 (Fixes + navbar component + app.js + server .js.)
-//const rubberDucksRoutes = require('./routes/rubberDucks')
-=======
 
 const recipeRouter = require ('./routes/recipeRouter')
 const usersRoutes = require('./routes/user')
 
 
->>>>>>> af3cad6a5586370ace36b214601df4560e6f9630
 dotenv.config();
 
 // Constants
@@ -37,17 +27,8 @@ app.use((req, res, next) => {
 })
 
 // Routes
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 app.use('/api/recipe', recipeRouter)
->>>>>>> af3cad6a5586370ace36b214601df4560e6f9630
 app.use('/api/user', usersRoutes)
-app.use('/api/recipes', recipesRouter)
-=======
-app.use('/api/recipe', recipeRouter)
->>>>>>> e7edea3 (Fixes + navbar component + app.js + server .js.)
-
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
@@ -59,7 +40,3 @@ mongoose.connect(process.env.MONGO_URI)
   }).catch((err) => {
     console.log(err)
   });
-
-
-
-
