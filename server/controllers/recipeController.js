d31a47550b4b6d4c22d2c25bb6d0720e65551b50
MeitalShpigel14 <meitalshp9431@gmail.com>
2024-09-15 12:52:23 +0300

    fixed RecipeDB to Recipe in recipeController

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,8 +1,8 @@
-const RecipeDB = require('../models/RecipeModel');
+const Recipe = require('../models/RecipeModel');
 
 const getAllRecipes = async(req,res) => {
     try{
-        const recipes = await RecipeDB.find();
+        const recipes = await Recipe.find();
         res.status(200).json(recipes);
     }
     catch(err) {
@@ -13,7 +13,7 @@ const getAllRecipes = async(req,res) => {
 
 const getRecipe = async(req,res) => {
     try{
-        const recipe = await RecipeDB.findById(req.params.id);
+        const recipe = await Recipe.findById(req.params.id);
         res.status(200).json(recipe);
     }
     catch(err) {
@@ -42,7 +42,7 @@ const createRecipe = async (req, res) => {
 
     try {
          // adding the new recipe to the database
-        const recipe =  await RecipeDB.create({title, prepTime, imgLink, submissionTime, description, ingredients, prepSteps, tags, userId});
+        const recipe =  await Recipe.create({title, prepTime, imgLink, submissionTime, description, ingredients, prepSteps, tags, userId});
         // returning the created recipe and a success message
         res.status(200).json({ recipe });
     } catch (err) {
