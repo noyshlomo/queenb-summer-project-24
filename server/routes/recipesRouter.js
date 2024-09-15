a41f78ed2c0fefac691a6e41b40a738d2ce23156
Or-Jerbi <orjerbi99@gmail.com>
2024-09-15 14:03:12 +0300

    delete randomDuck and incorrect recipes files

--- server/routes/recipesRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const recipesRouter = express.Router();
-const {getAllRecipes} = require('../controllers/recipeController');
-
-recipesRouter.get('/',getAllRecipes);
-//router.get('/', getAllDucks)
-
-module.exports = recipesRouter;
