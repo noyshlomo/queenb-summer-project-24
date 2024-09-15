cf0851ee70b907ea1a9cc2b3b3f10975a44f6d87
Or Jerbi <96544670+Or-Jerbi@users.noreply.github.com>
2024-09-15 11:57:20 +0300

    Update HomePage.js

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './Home.module.css';
-// import RandomDuck from '../../components/RandomDuck/RandomDuck';
 import ViewRecipes from '../ViewRecipes/ViewRecipes';
 
 
