bcb700991857f2052e2780c40326245deaed7667
renaswaid <renaswaid1@gmail.com>
2024-09-17 19:13:25 +0300

    handling the conflict at index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './styles/global.css'; 
 import App from './App';
 import { UserProvider } from './context/UserContext';
+import { RecipesContextProvider } from './context/RecipesContext';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserProvider>
+  <RecipesContextProvider>
+  <UserProvider>
       <App /> 
     </UserProvider>
+    </RecipesContextProvider>
+
   </React.StrictMode>
 );
