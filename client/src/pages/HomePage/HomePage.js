2d2f10114eca5d36ba35bf1879b2c5ac533930c4
Or-Jerbi <orjerbi99@gmail.com>
2024-09-25 15:01:01 +0300

    App+Navbar+styles fixes

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import styles from './Home.module.css';
-import ViewRecipes from '../ViewRecipes/ViewRecipes';
 import DisplayRecipes from "../../components/DisplayRecipes/DisplayRecipes";
 
 
 const Home = () => {
-  return <div className={styles.home}><DisplayRecipes /> <ViewRecipes /></div>
-
+  return <div className={styles.home}><DisplayRecipes /></div>
 };
 
 export default Home;
