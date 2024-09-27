116b3c32a87bc4ea91abe4da759d36365093f3a4
MeitalShpigel14 <meitalshp9431@gmail.com>
2024-09-27 12:44:26 +0300

    final css for the morning of 27/9

--- client/src/components/MultiSelect/MultiSelect.js
+++ client/src/components/MultiSelect/MultiSelect.js
@@ -36,7 +36,8 @@ const MultiSelect = ({ label, value, onChange , emptyFields, fieldName }) => {
 
     return(
         <>
-        <label className= {styles.labelMulti}>{label}</label>
+        <div className={styles.containerFirst}>
+          <label className= {styles.labelMulti}>{label}</label>
           <div className={styles.containerMulti}>
             <Select 
                 name={fieldName}
@@ -67,10 +68,15 @@ const MultiSelect = ({ label, value, onChange , emptyFields, fieldName }) => {
                       maxWidth: '500px', 
 
                     }),
+                    label: (provided) => ({
+                      ...provided,
+                      fontWeight: 600,
+                    }),
                   }}
             />
             {isError && <span className={styles.errorText}>Required</span>} {/* if the field is empty, adding Required below it */}   
             </div>
+          </div>
         </>
     )
     
